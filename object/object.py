12df5dc26415b142b799686abd14c9aed505cd34
InterfaceGuy <david.rug98@icloud.com>
2021-05-31 21:02:05 +0200

    changes in scene.py: - implemented add(), wait(), make_keyframe method - document now doesn't need to be passed in object definition   --> might be causing trouble in the future because document is now class variable and is created prior to init!
    changes in object.py:
    - added Sphere(), Cube() class
    - objects visibility will now be keyframed to off at frame 0

--- object/object.py
+++ object/object.py
@@ -1,13 +1,42 @@
 import c4d
-
+from pydeationlib.scene.scene import Scene
 """
 TODO: find way for not having to pass doc
 """
-class C4DObject():
+
+
+class CObject():
     """abstract class for adding objects to scene"""
 
-    def __init__(self, doc):
-        obj = c4d.BaseObject(c4d.Onull)
+    def __init__(self, doc=Scene.document):
+
+        # check if primitive type changed by child class
+        if hasattr(self, "prim"):
+            self.obj = c4d.BaseObject(self.prim)
+        else:
+            self.obj = c4d.BaseObject(c4d.Onull)  # return null as default
+        # add object to project file
         self.doc = doc
-        doc.InsertObject(obj)
+        self.doc.InsertObject(self.obj)
+
+        # defaults visibility to hidden at frame 0
+        Scene.make_keyframe(self.obj, t_ids=c4d.ID_BASEOBJECT_VISIBILITY_EDITOR,
+                            dtypes=c4d.DTYPE_LONG, value=c4d.MODE_OFF, time=0)
+        Scene.make_keyframe(self.obj, t_ids=c4d.ID_BASEOBJECT_VISIBILITY_RENDER,
+                            dtypes=c4d.DTYPE_LONG, value=c4d.MODE_OFF, time=0)
+
         c4d.EventAdd()
+
+
+class Sphere(CObject):
+
+    def __init__(self):
+        self.prim = c4d.Osphere
+        super(Sphere, self).__init__()
+
+
+class Cube(CObject):
+
+    def __init__(self):
+        self.prim = c4d.Ocube
+        super(Cube, self).__init__()
--- scene/scene.py
+++ scene/scene.py
@@ -12,13 +12,17 @@ class Scene():
     NOTE: maybe use BatchRender in the future
     """
 
-    def __init__(self, project_name):
+    # create document as class variable so it is accessible in CObject class
+    document = BaseDocument()
+
+    def __init__(self, project_name, doc=document):
         # following code obsolete but kept as reference for future efforts
         """
         path = os.path.dirname(os.path.abspath(__file__))
         head, tail = os.path.split(path)
         print(tail)
         """
+        # read in project name
         self.project_name = project_name
         # use subclass name for scene's name for saving c4d project file
         self.scene_name = self.__class__.__name__
@@ -32,21 +36,69 @@ class Scene():
             os.mkdir(self.scene_path)
             print("path successfully created")
         except FileExistsError:
-            print("path already exists")
+            pass
+        #    print("path already exists")
         except PermissionError:
             print("permission denied")
         # except FileNotFoundError:
         #   print("wrong tail: " + tail)
 
-        self.document = BaseDocument()  # create document
-        self.document.SetDocumentName(self.scene_name)
-        InsertBaseDocument(self.document)  # insert document in menu list
+        doc.SetDocumentName(self.scene_name)
+        InsertBaseDocument(doc)  # insert document in menu list
+        self.time = 0
 
-        missingAssets = []
-        assets = []
-        SaveProject(self.document, c4d.SAVEPROJECT_ASSETS |
-                    c4d.SAVEPROJECT_SCENEFILE, self.scene_path, assets, missingAssets)
-
-    def save(self):
-        SaveProject(self.document, c4d.SAVEPROJECT_ASSETS |
+    def save(self, doc=document):
+        # save the scene to project file
+        SaveProject(doc, c4d.SAVEPROJECT_ASSETS |
                     c4d.SAVEPROJECT_SCENEFILE, self.scene_path, [], [])
+
+    def get_frame(self):
+        # returns the frame corresponding to the current time
+        fps = self.document.GetFps()
+        frame = fps * self.time
+        return frame
+
+    @staticmethod
+    def make_keyframe(baseobject, t_ids, dtypes, value, time):
+        # utility function for making keyframing less cluttered
+        # pass in the form of t_ids = [id1, id2, ...], dtypes = [dtype1, dtype2, ...] if more than one desc level
+        if type(t_ids) == list and type(dtypes) == list:
+            desc_levels = [c4d.DescLevel(t_id, dtype, 0)
+                           for t_id, dtype in zip(t_ids, dtypes)]
+        elif type(t_ids) == int and type(dtypes) == int:
+            desc_levels = [c4d.DescLevel(t_ids, dtypes, 0)]
+        else:
+            raise TypeError(
+                "both t_ids and dtypes have to be either list or int")
+        # get descId
+        descId = c4d.DescID(*desc_levels)
+        # find corresponding ctrack and if empty create it
+        track = baseobject.FindCTrack(descId)
+        if track == None:
+            track = c4d.CTrack(baseobject, descId)
+            # insert ctrack into objects timeline
+            baseobject.InsertTrackSorted(track)
+        # get curve of ctrack
+        curve = track.GetCurve()
+        # add key
+        key = curve.AddKey(c4d.BaseTime(time))["key"]
+        # assign value to key
+        if type(value) == int:
+            key.SetGeData(curve, value)
+        elif type(value) == float:
+            key.SetValue(curve, value)
+        else:
+            raise TypeError("value type must be int or float")
+
+        # update c4d
+        c4d.EventAdd()
+
+    def wait(self, seconds=1):
+        self.time += seconds
+
+    def add(self, baseobject):
+        # inserts the object into the scene at given point in time
+        self.make_keyframe(baseobject.obj, t_ids=c4d.ID_BASEOBJECT_VISIBILITY_EDITOR,
+                           dtypes=c4d.DTYPE_LONG, value=c4d.MODE_ON, time=self.time)
+        self.make_keyframe(baseobject.obj, t_ids=c4d.ID_BASEOBJECT_VISIBILITY_RENDER,
+                           dtypes=c4d.DTYPE_LONG, value=c4d.MODE_ON, time=self.time)
